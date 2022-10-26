276d3089054d7112c57ce6dc1d29cc5fcb3a34b4
Brian Koo <comc49@gmail.com>
2022-10-26 19:40:05 -0400

    added packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+
+let envFilePath = '.env.dev';
+let synchronizeBool = true;
+
+console.log(`Running in ${process.env.ENVIRONMENT}`)
+if (process.env.ENVIRONMENT === 'production') {
+  envFilePath = '.env.prod';
+  synchronizeBool = false;
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 8080,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: synchronizeBool, // don't use true for prod
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
